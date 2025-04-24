940037141ba0015d96cb69159d163cd807dc492a
Phoebe <118837133+cowboydan72@users.noreply.github.com.>
2025-04-23 20:47:24 -0600

    Adds my solution to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    //Make string into reversed array
+    let string = "";
+    string += words;
+    let letters = string.split("");
+    letters.reverse();
 
-};
+
+    const reversedString = letters.join("");
+    return reversedString;
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;

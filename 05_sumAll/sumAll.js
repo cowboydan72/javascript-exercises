65a8bc2f2b7314b031a51ea1f306a53c6ab9da0d
Phoebe <118837133+cowboydan72@users.noreply.github.com.>
2025-04-25 23:19:38 -0600

    Passes first 4 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,17 @@
 const sumAll = function(start, end) {
-    let sum = 0
-    for (let i = start; i <= end; i++) {
-        sum = sum + i;
+    let sum = 0;
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum = sum + i;
+        };  
+    }
+    else if (start <= 0 || end <= 0) {
+        sum = "ERROR"
+    }
+    else {
+        for (let i = start; i <= end; i++) {
+            sum = sum + i;
+        };
     };
     return sum;
 };
